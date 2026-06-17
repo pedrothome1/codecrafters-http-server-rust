55e4471b32d0e21fbb77cf9785abaa46ed03c158
pedrothome1 <pedrothome2@outlook.com>
2026-06-17 20:54:14 -0300

    read headers and implement /user-agent route
    For now we're not reading the request body. Should we refactor now?

--- src/main.rs
+++ src/main.rs
@@ -11,25 +11,40 @@ fn main() -> Result<(), Box<dyn Error>> {
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                let mut request = String::new();
-
                 let mut reader = BufReader::new(&stream);
-                reader.read_line(&mut request)?;
+
+                let mut request_line = String::new();
+                let mut user_agent = String::new();
+
+                reader.read_line(&mut request_line)?;
+
+                loop {
+                    let mut header = String::new();
+                    reader.read_line(&mut header)?;
+
+                    if header == "\r\n" {
+                        break;
+                    }
+
+                    let mut split = header.splitn(2, ':');
+                    let (name, value) = (split.next().unwrap(), split.next().unwrap());
+
+                    if name.eq_ignore_ascii_case("User-Agent") {
+                        user_agent = value.trim().to_string();
+                    }
+                }
 
                 // example: GET /index.html HTTP/1.1
-                let tokens = request.splitn(3, ' ').collect::<Vec<&str>>();
+                let tokens = request_line.splitn(3, ' ').collect::<Vec<&str>>();
                 let path = tokens[1];
 
                 let response = if path == "/" {
                     "HTTP/1.1 200 OK\r\n\r\n".to_string()
                 } else if path.starts_with("/echo/") {
                     let string = path[1..].splitn(2, '/').skip(1).next().unwrap();
-
-                    format!(
-                        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                        string.len(),
-                        string
-                    )
+                    build_content_response("200 OK", "text/plain", string)
+                } else if path == "/user-agent" {
+                    build_content_response("200 OK", "text/plain", &user_agent)
                 } else {
                     "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
                 };
@@ -43,4 +58,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
+}
+
+fn build_content_response(status: &str, content_type: &str, content: &str) -> String {
+    format!(
+        "HTTP/1.1 {}\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}",
+        status,
+        content_type,
+        content.len(),
+        content
+    )
 }
