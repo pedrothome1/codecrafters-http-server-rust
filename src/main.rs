272054303e6b39ee11d02407b4bcf9cda789942d
pedrothome1 <pedrothome2@outlook.com>
2026-06-17 19:07:27 -0300

    serve 200 OK response

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
-#[allow(unused_imports)]
+use std::error::Error;
+use std::io::Write;
 use std::net::TcpListener;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+fn main() -> Result<(), Box<dyn Error>> {
+    let listener = TcpListener::bind("127.0.0.1:4221")?;
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n")?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    Ok(())
 }
