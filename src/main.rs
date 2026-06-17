38f7e9abdcf55c4e5b1f0bf37a44a84dad983666
pedrothome1 <pedrothome2@outlook.com>
2026-06-17 19:37:33 -0300

    implement simple request processing
    Here we should not rely on reader functions that expect an EOF signal.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
 use std::error::Error;
-use std::io::Write;
+use std::io::{BufRead, BufReader, Write};
 use std::net::TcpListener;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let listener = TcpListener::bind("127.0.0.1:4221")?;
+    let addr = "127.0.0.1:4221";
+    let listener = TcpListener::bind(addr)?;
+
+    eprintln!("Running server at {addr}");
 
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n")?;
+                let mut request = String::new();
+
+                let mut reader = BufReader::new(&stream);
+                reader.read_line(&mut request)?;
+
+                // example: GET /index.html HTTP/1.1
+                let tokens = request.splitn(3, ' ').collect::<Vec<&str>>();
+                let path = tokens[1];
+
+                let response = if path == "/" {
+                    "HTTP/1.1 200 OK\r\n\r\n"
+                } else {
+                    "HTTP/1.1 404 Not Found\r\n\r\n"
+                };
+
+                stream.write_all(response.as_bytes())?;
             }
             Err(e) => {
                 println!("error: {}", e);
